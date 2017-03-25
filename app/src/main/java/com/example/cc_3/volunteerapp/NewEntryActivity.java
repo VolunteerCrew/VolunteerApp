9d0419f6336d75734ae47949aa700c8e303d0528
CannyOne057 <PM57mobile@gmail.com>
2017-03-24 18:37:00 -0600

    Added new data fields

--- app/src/main/java/com/example/cc_3/volunteerapp/NewEntryActivity.java
+++ app/src/main/java/com/example/cc_3/volunteerapp/NewEntryActivity.java
@@ -10,6 +10,11 @@ public class NewEntryActivity extends AppCompatActivity {
 
     private EditText mSponsorField;
     private EditText mTitleField;
+    private EditText mAddressField;
+    private EditText mPhoneField;
+    private EditText mDescriptionField;
+    private EditText mCreatedField;
+
 
     private Button mUploadButton;
 
@@ -25,6 +30,11 @@ public class NewEntryActivity extends AppCompatActivity {
         // Grabbing references to the EditTexts inside the layout
         mSponsorField = (EditText) findViewById(R.id.sponsor_field);
         mTitleField = (EditText) findViewById(R.id.title_field);
+        mAddressField = (EditText) findViewById(R.id.addr_field);
+        mPhoneField = (EditText) findViewById(R.id.phone_field);
+        mDescriptionField = (EditText) findViewById(R.id.desc_field);
+        mCreatedField = (EditText) findViewById(R.id.created);
+
 
         mUploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,14 +42,18 @@ public class NewEntryActivity extends AppCompatActivity {
 
                 String sponsor = mSponsorField.getText().toString();
                 String title = mTitleField.getText().toString();
+                String address = mAddressField.getText().toString();
+                String phone = mPhoneField.getText().toString();
+                String description = mDescriptionField.getText().toString();
+                String created = mCreatedField.getText().toString();
 
                 OppDataModel commData = new OppDataModel(
                         sponsor,
                         title,
-                        "7801 Candelaria Rd NE",
-                        "575-914-1826",
-                        "Help with Food Drive at Sandia High School",
-                        "Tomorrow",
+                        address,
+                        phone,
+                        description,
+                        created,
                         "The Day After Tomorrow"
                 );
 
