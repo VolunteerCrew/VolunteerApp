9003690b2a38c66ff3abef274b5089049444e8ad
Bjorkbat <bjorkbat@gmail.com>
2017-03-24 20:28:20 -0600

    Can now pull data from database

--- app/src/main/java/com/example/cc_3/volunteerapp/OrganizationFragment.java
+++ app/src/main/java/com/example/cc_3/volunteerapp/OrganizationFragment.java
@@ -5,10 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +29,10 @@ public class OrganizationFragment extends Fragment {
 
     FloatingActionButton mAddButton;
 
+    ListView mList;
+    ArrayAdapter mAdapter;
+    ArrayList<String> mTitles;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +40,11 @@ public class OrganizationFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_organization, container, false);
 
         mAddButton = (FloatingActionButton) rootView.findViewById(R.id.add_new_opportunity);
+
+        mList = (ListView) rootView.findViewById(R.id.list);
+        mTitles = new ArrayList<>();
+        mAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, mTitles);
+        mList.setAdapter(mAdapter);
 
         mAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +63,46 @@ public class OrganizationFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseDatabase database = ((MainActivity) getActivity()).mDatabase;
+
+        database.getReference("organization").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // Temporarily store the data inside something called an Iterable
+                Iterable<DataSnapshot> opportunities = dataSnapshot.getChildren();
+
+                // Just zero out all the old data
+                mTitles.clear();
+
+                // Iterate through the iterable, and for each animal add them to the mAnimals
+                // ArrayList
+                HashMap<String, Object> child;
+                for (DataSnapshot opportunity : opportunities) {
+
+                    child = (HashMap<String, Object>) opportunity.getValue();
+
+                    // Add
+                    mTitles.add(child.get("title").toString());
+
+                }
+
+                // Because we zero'd out the data, we need to tell the Adapter that we changed
+                // the original data
+                mAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
