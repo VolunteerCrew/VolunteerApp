c6d9a44a6ee6ee54483e07fbb3a94b7b4df5812d
Peter D Meredith <PM57mobile@gmail.com>
2017-03-26 09:58:35 -0600

    Adding code from Organization to Sports and Grassroots Classes

--- app/src/main/java/com/example/cc_3/volunteerapp/GrassrootsFragment.java
+++ app/src/main/java/com/example/cc_3/volunteerapp/GrassrootsFragment.java
@@ -1,5 +1,6 @@
 package com.example.cc_3.volunteerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -7,9 +8,16 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by PM57M on 3/23/2017.
@@ -19,44 +27,70 @@ public class GrassrootsFragment extends Fragment {
 
     FloatingActionButton mAddButton;
 
+    ListView mList;
+    ArrayAdapter mAdapter;
+    ArrayList<String> mTitles;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_sports, container, false);
 
         mAddButton = (FloatingActionButton) rootView.findViewById(R.id.add_new_opportunity);
+
+        mList = (ListView) rootView.findViewById(R.id.list);
+        mTitles = new ArrayList<>();
+        mAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, mTitles);
+        mList.setAdapter(mAdapter);
+
         mAddButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                String key = ((MainActivity) getActivity()).mDatabase.getReference().push().getKey();
-
-                OppDataModel commData = new OppDataModel(
-                        "John Smith",
-                        "Yard Cleaning",
-                        "9183 Saturn St NE",
-                        "505-358-5772",
-                        "Clean up Mrs Smith's backyard",
-                        "Tomorrow",
-                        "The Day After Tomorrow"
-                );
-
-                Map<String, Object> commValues = commData.toMap();
-
-                Map<String, Object> childUpdates = new HashMap<>();
-                childUpdates.put("/grassroots/" + key, commValues);
-                ((MainActivity) getActivity()).mDatabase.getReference()
-                        .updateChildren(childUpdates);
-
-                // These two below are just left behind as examples
-                //childUpdates.put("/grassroots/" + key, commValues);
-                //childUpdates.put("/sports-events/" + key, commValues);
+                // transition to the NewEntryActivity
+                Intent intent = new Intent(getActivity(), NewEntryActivity.class);
+                // give it some extra data to establish here to upload data to).
+                intent.putExtra ("FRAGMENT", GrassrootsFragment.class.getSimpleName());
+                int requestCode = (((MainActivity) getActivity())).NEW_ENTRY_ACTIVITY_CODE;
+                startActivityForResult(intent, requestCode);
             }
 
         });
         return rootView;
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseDatabase database = ((MainActivity) getActivity()).mDatabase;
+
+        database.getReference("grassroots").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // store the data inside an Iterable
+                Iterable<DataSnapshot> opportunities = dataSnapshot.getChildren();
+                // zeros out the old data
+                mTitles.clear();
+                // iterates through the list of data objects and adds them to the ArrayList
+                HashMap<String, Object> child;
+                for (DataSnapshot opportunity : opportunities) {
+                    child = (HashMap<String, Object>) opportunity.getValue();
+                    // add
+                    mTitles.add(child.get("title").toString());
+                }
+                // because we zero'd out the data, we need to tell the Adapter
+                // that we changed the original data
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cc_3/volunteerapp/SportsFragment.java
+++ app/src/main/java/com/example/cc_3/volunteerapp/SportsFragment.java
@@ -1,5 +1,6 @@
 package com.example.cc_3.volunteerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -7,7 +8,15 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,42 +28,72 @@ public class SportsFragment extends Fragment {
 
     FloatingActionButton mAddButton;
 
+    ListView mList;
+    ArrayAdapter mAdapter;
+    ArrayList<String> mTitles;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_sports, container, false);
 
         mAddButton = (FloatingActionButton) rootView.findViewById(R.id.add_new_opportunity);
+
+        mList = (ListView) rootView.findViewById(R.id.list);
+        mTitles = new ArrayList<>();
+        mAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1);
+        mList.setAdapter(mAdapter);
+
         mAddButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                String key = ((MainActivity) getActivity()).mDatabase.getReference().push().getKey();
+                // transition to the NewEntryActivity
+                Intent intent = new Intent(getActivity(), NewEntryActivity.class);
+                // Give it some data to inform it which activity is being called
+                // and where to upload the data to
+                intent.putExtra("FRAGMENT", SportsFragment.class.getSimpleName());
 
-                OppDataModel commData = new OppDataModel(
-                        "Jose Vargas",
-                        "ABQ Little League",
-                        "7892 Able Rd SE",
-                        "505-358-5772",
-                        "Need Neutral Umpire for Championship Match",
-                        "Tomorrow",
-                        "The Day After Tomorrow"
-                );
+                int requestCode = ((MainActivity) getActivity()).NEW_ENTRY_ACTIVITY_CODE;
+                startActivityForResult(intent, requestCode);
 
-                Map<String, Object> commValues = commData.toMap();
 
-                Map<String, Object> childUpdates = new HashMap<>();
-                childUpdates.put("/sports/" + key, commValues);
-                ((MainActivity) getActivity()).mDatabase.getReference()
-                        .updateChildren(childUpdates);
-
-                // These two below are just left behind as examples
-                //childUpdates.put("/grassroots/" + key, commValues);
-                //childUpdates.put("/sports-events/" + key, commValues);
             }
         });
         return rootView;
         }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseDatabase database = ((MainActivity) getActivity()).mDatabase;
+        database.getReference("sports").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // stores the data in an Iterable
+                Iterable<DataSnapshot> opportunities = dataSnapshot.getChildren();
+                // zero's out the old data
+                mTitles.clear();
+                // Iterate through the data then add it to the ArrayList
+                HashMap<String, Object> child;
+                for (DataSnapshot opportunity : opportunities) {
+                    child = (HashMap<String, Object>) opportunity.getValue();
+                    // Add
+                    mTitles.add(child.get("title").toString());
+                }
+                // Then, because we zero'd out the data, we tell the Adapter the data has changed
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+    }
 }
