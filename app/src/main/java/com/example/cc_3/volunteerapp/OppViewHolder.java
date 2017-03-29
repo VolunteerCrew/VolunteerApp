1c250ddc8fbaca255268c79b3aba33c1ec7f4f8b
CannyOne057 <PM57mobile@gmail.com>
2017-03-28 18:51:55 -0600

    View Holder filled out

--- app/src/main/java/com/example/cc_3/volunteerapp/OppViewHolder.java
+++ app/src/main/java/com/example/cc_3/volunteerapp/OppViewHolder.java
@@ -5,8 +5,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 /**
  * Created by johnborden on 3/28/17.
  */
@@ -14,15 +12,33 @@ import org.w3c.dom.Text;
 public class OppViewHolder {
 
     TextView mSponsor;
+    TextView mTitles;
+    TextView mDescription;
+    TextView mAddress;
+    TextView mPhoneNumber;
+    TextView mDateCreated;
+    TextView mDateExpires;
 
     public OppViewHolder(Context context, ViewGroup parent, View convertView) {
 
         mSponsor = (TextView) convertView.findViewById(R.id.sponsor);
+        mTitles = (TextView) convertView.findViewById(R.id.title);
+        mDescription = (TextView) convertView.findViewById(R.id.description);
+        mAddress = (TextView) convertView.findViewById(R.id.address);
+        mPhoneNumber = (TextView) convertView.findViewById(R.id.phoneNumber);
+        mDateCreated = (TextView) convertView.findViewById(R.id.dateCreated);
+        mDateExpires = (TextView) convertView.findViewById(R.id.dateExpires);
     }
 
     public void populateLayout(OppDataModel opp) {
 
         mSponsor.setText(opp.sponsor);
+        mTitles.setText(opp.title);
+        mDescription.setText(opp.description);
+        mAddress.setText(opp.address);
+        mPhoneNumber.setText(opp.phoneNumber);
+        mDateCreated.setText(opp.dateCreated);
+        mDateExpires.setText(opp.dateExpires);
 
     }
 
